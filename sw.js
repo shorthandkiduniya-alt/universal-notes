9f224b9922b984d644f5744ea4af961d0a7c52c4
shorthandkiduniya-alt <shorthandkiduniya@gmail.com>
2026-06-10 20:30:39 +0530

    Update service worker cache and asset handling

--- sw.js
+++ sw.js
@@ -1,10 +1,29 @@
-const CACHE_NAME = 'note-app-v2';
-const ASSETS = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'quicknotes-v2';
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+const FILES = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(FILES))
+    );
+
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+
+            return response || fetch(event.request);
+
+        })
+    );
+
 });
